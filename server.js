b22ae8c46c56825f92c9ee630586d3f92c144fba
HCook <166083664+Hayden-Cook@users.noreply.github.com>
2024-10-07 19:07:01 -0500

    linked handlebars and created messages.js

--- server.js
+++ server.js
@@ -1,31 +1,44 @@
+require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
-require('dotenv').config();
+const {engine} = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+// Set up Handlebars.js engine with custom helpers
 
 const sess = {
-  secret: process.env.SESSION_SECRET,
+  secret: process.env.SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
 
+// Inform Express.js on which template engine to use
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-// app.use(routes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log("Now listening " + PORT));
+  app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
 });
