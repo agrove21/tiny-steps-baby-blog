16cf8075730142dbfaf51903caa990f7d4c66540
Your Name <you@example.com>
2024-10-10 18:45:15 -0500

    updated the welcome page, header and description

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,19 +1,40 @@
 const router = require("express").Router();
 
+router.get("/", async (req, res) => {
+  res.render("Welcome", {
+    title: "Welcome",
+    description: "Celebrate and capture every step of your baby's journey, from milestones to cherished moments. Share these precious memories with loved ones and document the unforgettable early years of your little one’s life.",
+  });
+});
+
 router.get("/blog", async (req, res) => {
-  res.render("blog", {title: "Tiny Tales", layout: "dashboard"});
+  res.render("blog", {
+    title: "Tiny Tales",
+    description: "Welcome to your little one's diary, a place to capture and cherish every precious memory.",
+    layout: "dashboard",
+  });
 });
 
 router.get("/first", async (req, res) => {
-    res.render("first", {title: "Firsts and Milestones", layout: "dashboard"});
-  });
+  res.render("first", { 
+    title: "Firsts and Milestones",
+    description: "From the first smile to the first steps, capture and track your little one's milestones here.",
+    layout: "dashboard" });
+});
 
 router.get("/gallery", async (req, res) => {
-    res.render("gallery", {title: "Photo Gallery", layout: "dashboard"});
-  });
+  res.render("gallery", { 
+    title: "Snapshot Memories", 
+    description: "Each snapshot captures the joy and love of your little one's journey, from special moments to everyday adventures.", 
+    layout: "dashboard" });
+});
 
-  router.get("/advice", async (req, res) => {
-    res.render("advice", {title: "Parenting Insights", layout: "dashboard"});
+router.get("/advice", async (req, res) => {
+  res.render("advice", {
+    title: "Parenting Insights",
+    description: "Get helpful tips and expert advice to guide you through your parenting journey.",
+    layout: "dashboard",
   });
+});
 
 module.exports = router;
