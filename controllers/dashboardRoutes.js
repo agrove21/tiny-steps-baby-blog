9c9e9655434927d078bfb90b1e16b8d073eb3470
LAUREN <lv.dean75@gmail.com>
2024-10-09 22:34:20 -0400

    added advice page

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -12,5 +12,8 @@ router.get("/gallery", async (req, res) => {
     res.render("gallery", {title: "Photo Gallery", layout: "dashboard"});
   });
 
+  router.get("/advice", async (req, res) => {
+    res.render("advice", {title: "Parenting Insights", layout: "dashboard"});
+  });
 
 module.exports = router;
