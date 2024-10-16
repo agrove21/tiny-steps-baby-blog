7de791ec0455e5402e7a93e0cf36f7183e4fdfff
Your Name <you@example.com>
2024-10-16 11:23:08 -0500

    updated padding and debugged welcome route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 
 router.get("/", async (req, res) => {
   if (req.session.logged_in){
-    res.redirect("/dashboard/welcome");
+    res.redirect("/dashboard");
     return
   }
 
